7f16c4313349eff35e41d7e0fee2cf302a6ff0ff
brahimRizq <brahimrizq@gmail.com>
2023-03-27 03:37:16 +0000

    fix namespace

--- src/Common/Infrastructure/Http/BaseApiController.php
+++ src/Common/Infrastructure/Http/BaseApiController.php
@@ -5,11 +5,12 @@ namespace CQRS\Common\Infrastructure\Http;
 use CQRS\Common\Infrastructure\Model\Database\MongodbClient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class BaseApiController extends AbstractController
 {
-    public function version(): Response
+    public function version(Request $request): Response
     {
         return new JsonResponse(
             [
