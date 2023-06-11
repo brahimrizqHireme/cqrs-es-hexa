9123000d8ae9502a2a458265d122dd36c142df2d
brahimrizq <brahim@wiggli.io>
2023-06-11 16:57:21 +0100

    sasas

--- tests/Api/Functional/Product/ProductTest.php
+++ tests/Api/Functional/Product/ProductTest.php
@@ -25,7 +25,7 @@ class ProductTest extends KernelTestCase
         parent::setUp();
         self::bootKernel();
         $this->container = self::getContainer();
-        $this->client = HttpClient::create(['base_uri' => 'http://localhost']);
+        $this->client = HttpClient::create(['base_uri' => $_SERVER['BASE_URL']]);
     }
 
     private function sendARequest(
